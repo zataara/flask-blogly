e2183dee7d8028e9643524393e332c75c2f2a0d8
zataara <bryantmac@gmail.com>
2021-06-19 13:43:27 -0700

    created user model

--- models.py
+++ models.py
@@ -1,1 +1,31 @@
-"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+        db.app = app
+        db.init_app(app)
+
+
+class User(db.Model):
+    '''Database model for Users'''
+
+    __tablename__ = 'users'
+
+    def __repr__(self):
+        '''Show additional info about the user'''
+        u = self
+        return f'<User {u.id} {u.first_name} {u.last_name} {u.img_url}'
+
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement = True)
+    first_name = db.Column(db.String(30),
+                            nullable=False)
+    last_name = db.Column(db.String(30),
+                            nullable=False)
+    img_url = db.Column(db.String)
+
+
+
+
