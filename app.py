0960b8feca15ce936e55c15dc8dfe8b8c5c3ce36
zataara <bryantmac@gmail.com>
2021-06-22 14:52:16 -0700

    adjusting form submission and route info

--- app.py
+++ app.py
@@ -25,14 +25,20 @@ def show_user(user_id):
     user = User.query.get_or_404(user_id)
     return render_template('user_details.html', user=user)
 
-@app.route('/', methods=['POST'])
+
+@app.route('/new', methods=["GET"])
+def show_new_user_form():
+    return render_template('create_user.html')
+
+@app.route('/new', methods=['POST'])
 def add_user():
+    '''Handle the form submission for creating a new user'''
     first_name = request.form['first_name']
     last_name = request.form['last_name']
-    img_url = request.form['img_url']
+    img_url = request.form['img_url'] or None
 
     new_user = User(first_name=first_name, last_name=last_name, img_url=img_url)
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(f"/{new_user.id}")
+    return redirect('/')
